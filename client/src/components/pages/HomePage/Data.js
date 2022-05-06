0ad790e21d66821eaf2f070a41249635094983ed
nour <nourazouzi0@gmail.com>
2022-05-06 13:49:02 +0100

    page d'acceuil

--- client/src/components/pages/HomePage/Data.js
+++ client/src/components/pages/HomePage/Data.js
@@ -2,7 +2,7 @@ export const homeObjOne = {
   lightBg: true,
   lightText: false,
   lightTextDesc: false,
-  topLine: "Graiet fete son 64 anniversaire",
+  topLine: "Graiet fete son 64 anniversaire aujourd'hui",
   headLine: "Visitez notre boutique Graiet Electromenager Msaken",
   description:
     "Graiet vous offre un pack CL comportant un refrigerateur machine a laver et cuisiniere avec un prix interressant Visiter notre boutique a Msaken et profiter de ce pack ",
